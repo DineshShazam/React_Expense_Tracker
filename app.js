59072480583a657ebc46397fbf86fa4700aa7a00
DineshShazam <dineshdinu1607@yahoo.com>
2020-08-01 01:42:49 +0530

    build changes

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ app.use('/',router)
 
 // if(process.env.NODE_ENV == 'production') {
 
-    app.use(express.static(path.join(__dirname,'../../Client/build')))
+    app.use(express.static(path.join(__dirname,'./Client/build')))
 
-    app.get('*',(req,res) => { res.sendFile(path.join(__dirname,'../../Client/build/index.html')) })
+    app.get('*',(req,res) => { res.sendFile(path.join(__dirname,'./Client/build/index.html')) })
 
 //}
 
