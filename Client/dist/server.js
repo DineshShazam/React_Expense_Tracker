5a263080eba713dab815798a2e994ebac19e2078
DineshShazam <dineshdinu1607@yahoo.com>
2020-08-01 00:53:49 +0530

    build changes

--- Client/dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
-var _app = _interopRequireDefault(require("./app"));
-
-var _colors = _interopRequireDefault(require("colors"));
-
-_app["default"].listen(process.env.PORT, console.log("Application running in ".concat(process.env.PORT).white.bold));
