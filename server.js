4a1095731e974f2576d1ea4c1be7f54ecede4176
DineshShazam <dineshdinu1607@yahoo.com>
2020-08-01 02:05:01 +0530

    .

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ import app from './app'
 import colors from 'colors'
 
 
-app.listen(process.env.PORT,console.log(`Application running in ${process.env.PORT}`.white.bold))
+app.listen(process.env.PORT || 5000,console.log(`Application running in ${process.env.PORT || 5000}`.white.bold))
